41c9fdf4b04c0ab864c6b21967ea237da7c52317
Harsh-Prasad09 <immortalharsh61@outlook.com>
2025-04-03 00:04:43 +0530

    port

--- Backend/main.py
+++ Backend/main.py
@@ -152,4 +152,4 @@ def predict_text():
 # -------------------- Run the Flask App --------------------
 if __name__ == '__main__':
     # app.run(host="0.0.0.0", port=5000, debug=True)
-    serve(app, '0.0.0.0', port = 5000)
+    serve(app, '0.0.0.0', port = 8080)
